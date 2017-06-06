a9f6e8638db108299da260321b454751e0b8906f
Simon Arida <me@Simons-MacBook-Pro.local>
2017-06-06 12:34:17 +0300

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str);
+
+var string = "hello"
+
+print(string)
+
+string = "simon"
+
+print(string)
+
+var name = "Simon"
+var food = "Chiken"
+
+print("\(name) likes to eat \(food)" )
+
+var score = 10
+
+var add = 5
+
+score += add * 2
+print (score)
+
+print("Final Score: \(score)")
+
+
+var double1: Double = 1.1
+
+var double2: Double = 2.2
+
+print(double1 + double2)
+
+var state = true
+var state1 = false
+
+var state2 = state1
+
+print(String(state))
+
+
+

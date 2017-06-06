3b639d1351df9d77a0be3b6984902f561ee47c0f
Simon Arida <me@Simons-MacBook-Pro.local>
2017-06-06 13:06:30 +0300

    arrays

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var fruits = ["Apple","Banana","Melon","Pear"]
+
+fruits[0]
+fruits[1]
+fruits[2]
+fruits[3]
+
+fruits.append("Lemon")
+
+fruits[4]
+fruits.count
+
+fruits.insert("Orange", at: 1)
+
+fruits.count
+
+fruits[1]
+
+fruits.remove(at: 2)
+
+fruits.count
+print(fruits)
+
+fruits.sort()
+
+print(fruits)
